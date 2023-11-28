401892bac500b50b08ffca26933ed0263727cac1
Agustin Barraza <agus.barraza@hotmail.com>
2023-11-27 22:58:29 -0300

    Agregamos el checkadmin a la ruta de userspremium

--- src/routs/user.router.js
+++ src/routs/user.router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from '../controllers/user.controllers.js';
 import { checkAuth } from '../middlewares/authJwt.js';
 import { upload } from '../middlewares/multer.js';
+import { checkAdmin } from '../middlewares/checkAdmin.js';
 const controller = new UserController();
 
 
@@ -11,7 +12,7 @@ router.get('/deleteUsers',controller.deleteUsers)
 router.post('/register',controller.register);
 router.post('/login',controller.login);
 router.get('/logout',checkAuth,controller.logout);
-router.post('/premium/:idUser',controller.updatePremium);
+router.post('/premium/:idUser',checkAdmin,controller.updatePremium);
 router.post('/upload',checkAuth,upload.fields([{ name: 'profile', maxCount: 1 }, { name: 'ident', maxCount: 1 },{ name: 'domicilio', maxCount: 1 },{ name: 'cuenta', maxCount: 1 }, { name: 'product', maxCount: 3 }]),controller.documentUpload);
 router.post('/add/:idProd/quantity/:quantity',checkAuth,controller.addProdToUserCart);
 router.post('/reset-pass', checkAuth, controller.resetPass);
